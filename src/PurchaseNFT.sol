cb0a9739aaf6d59167d01dd3e329714ec988c804
anchabadze <zurab.anchabadze@gmail.com>
2025-04-05 15:29:03 -0300

    finished

---
+++ src/PurchaseNFT.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract PurchaseNFT is ERC721("myNFT", "myNFT"), Ownable(msg.sender) {
+    using SafeERC20 for IERC20;
+
+    IERC20 public paymentToken;
+    uint256 public mintPrice;
+    uint256 public curSupply = 1;
+    uint256 public constant maxSupply = 1000;
+
+    constructor(address _paymentToken, uint256 _mintPrice) {
+        paymentToken = IERC20(_paymentToken);
+        mintPrice = _mintPrice;
+    }
+
+    function mint() external {
+        require(curSupply < maxSupply, "max supply reached");
+        require(paymentToken.balanceOf(msg.sender) >= mintPrice, "not enough tokens");
+        paymentToken.safeTransferFrom(msg.sender, address(this), mintPrice);
+        _safeMint(msg.sender, curSupply);
+        curSupply += 1;
+    }
+
+    function setMintPrice(uint256 _newMintPrice) external onlyOwner {
+        mintPrice = _newMintPrice;
+    }
+
+    function withdrawERC20() external onlyOwner {
+        paymentToken.safeTransfer(owner(), paymentToken.balanceOf(address(this)));
+    }
+
+    function getPrice() external view returns (uint256) {
+        return mintPrice;
+    }
+
+    function getRemainingSupply() external view returns (uint256) {
+        return maxSupply - curSupply;
+    }
+}
---
+++ test/PurchaseNFT.t.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {Test} from "@forge-std/src/Test.sol";
+import {console} from "@forge-std/src/console.sol";
+import {PurchaseNFT} from "../src/PurcahseNFT.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockERC20 is ERC20 {
+    constructor() ERC20("Mock Token", "MT") {
+        _mint(msg.sender, 1_000_000 ether);
+    }
+}
+
+contract TEST_PurchaseNFT is Test {
+    PurchaseNFT public nft;
+    MockERC20 public token;
+
+    address owner = makeAddr("owner");
+    address user1 = makeAddr("user1");
+    address user2 = makeAddr("user2 ");
+
+    function setUp() public {
+        vm.startPrank(owner);
+        token = new MockERC20();
+        nft = new PurchaseNFT(address(token), 10 ether);
+        token.transfer(user1, 1000 ether);
+        token.transfer(user2, 1000 ether);
+        vm.stopPrank();
+    }
+
+    function test_mintNFTfailedDueToInsufficientAllowance() public {
+        vm.startPrank(user1);
+        vm.expectRevert();
+        nft.mint();
+        vm.stopPrank();
+    }
+
+    function test_mintNFTSucceded() public {
+        vm.startPrank(user1);
+        token.approve(address(nft), 10 ether);
+        nft.mint();
+        vm.stopPrank();
+
+        assertEq(nft.ownerOf(1), user1);
+    }
+
+    function test_withdrawERC20FailedDueToNotOwner() public {
+        _usersBuyingNFT();
+        vm.startPrank(user1);
+        vm.expectRevert();
+        nft.withdrawERC20();
+        vm.stopPrank();
+    }
+
+    function test_withdrawERC20() public {
+        _usersBuyingNFT();
+
+        vm.startPrank(owner);
+        nft.withdrawERC20();
+
+        vm.assertEq(token.balanceOf(owner), 1_000_000 ether - 1000 ether - 1000 ether + 20 ether);
+    }
+
+    function _usersBuyingNFT() internal {
+        vm.startPrank(user1);
+        token.approve(address(nft), 10 ether);
+        nft.mint();
+        vm.stopPrank();
+
+        vm.startPrank(user2);
+        token.approve(address(nft), 10 ether);
+        nft.mint();
+        vm.stopPrank();
+    }
+}
